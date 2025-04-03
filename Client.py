6920abdd09859b206b77d1f022579fb561a37861
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2025-04-03 23:04:41 +0800

    feat: add code commenting

--- Client.py
+++ Client.py
@@ -8,29 +8,34 @@ import os
 import json
 
 import msvcrt
-
 import sys
-
 import winsound
+
+# Import required libraries
 
 ROWS, COLS = 6, 20
 CAR_SYMBOL, OBSTACLE_SYMBOL, EMPTY_SYMBOL = 1, 2, 0
 
-game_ended = False
+# constant variables for grid size and integer representations of grid
 
-conn_det = []
+game_ended = False # Boolean value for whether the game has ended
 
+conn_det = [] # The current connection details (globally accessible)
+
+# The cars from which we can choose
 car_select_array = ['🚃','🚋','🚓', '🚔', '🚗', '🚘', '🚙', '🏎️', '🚡', '🚞', '🚲', '🏍️', '🛵', '⛴️', '🚢']
 
-
+# The elements which form the drawn grid
 elements = [
     "  ",
     car_select_array[1],
     "🧱",
 ]
 
+# Create a new empty grid
 grid = np.full((ROWS, COLS), EMPTY_SYMBOL, dtype=int)
 
+# Do all our logging stuff
 import logging
 
 logger = logging.getLogger("client")
@@ -38,28 +43,28 @@ logging.basicConfig(filename='client.log', encoding='utf-8', level=logging.DEBUG
 
 
 def CheckKeys(sock: sock.SocketType, kill_queue: mp.Queue, name: str):
-    mp.Process(target=GameLoop, args=(sock,kill_queue, name, elements)).start()
+    mp.Process(target=GameLoop, args=(sock,kill_queue, name, elements)).start() # Start the game loop checking for messages from the server
     global game_ended
     while True:
-        if kill_queue.empty() == False:
+        if kill_queue.empty() == False: # Check if it is time to terminate
             kill = kill_queue.get_nowait()
             if kill == True:
                 game_ended = True
                 return
 
-        if msvcrt.kbhit():
+        if msvcrt.kbhit(): # Check for key inputs
                     character = msvcrt.getch()
                     if character == b"\xe0" or character == b"\x00":
                         character = msvcrt.getch()
 
-                        if character == b"H":
+                        if character == b"H": # If up, send up to the server
                             sock.sendall(b"UP")
-                        elif character == b"P":
+                        elif character == b"P": # Vice versa
                             sock.sendall(b"DOWN")
 
 
 def ConnectToServer(ip: str, port: int, name: str):
-    while True:
+    while True: # Always try to make first contact to the server quietly
          try:
             clientsocket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
             clientsocket.connect((ip, port))
@@ -72,78 +77,83 @@ def ConnectToServer(ip: str, port: int, name: str):
          except:
               pass
 
-
+# Actually start the game
 def GameLoop(sock: sock.SocketType, kill_queue: mp.Queue, name: str, elements: list):
     global game_ended
-    if kill_queue.empty() == False:
+    if kill_queue.empty() == False: # Check for termination
             kill = kill_queue.get_nowait()
             if kill == True:
                 return
+    # Cool music
     winsound.PlaySound("assets/game.wav", winsound.SND_FILENAME + winsound.SND_ASYNC + winsound.SND_LOOP)
+    
+    # Create enough space for the grid to be drawn without overwriting console text
     for i in range(0, ROWS*2+1):
         print()
 
     while True:
-        data = sock.recv(4096)
+        data = sock.recv(4096) # Upon data being recieved
         if data:
             logger.info(f"[CLIENT] Received data: {data}")
-            if b"LOSE" in data:
+            if b"LOSE" in data: # Stop the game if we lose
                 print(f"You lost, {name}! Your score was {data.decode('utf-8')[5:]}")
                 kill_queue.put_nowait(True)
                 game_ended = True
                 sock.close()
-                winsound.PlaySound(None, winsound.SND_PURGE)
+                winsound.PlaySound(None, winsound.SND_PURGE) # Stop music, close sockets, put True into kill_queue
                 break
-            for line in data.decode('utf-8').splitlines():
-                game_grid = (np.asarray(json.loads(line), dtype=int))
+            for line in data.decode('utf-8').splitlines(): # Print each individual line of the server data
+                game_grid = (np.asarray(json.loads(line), dtype=int)) # Load the lines from the json data
                 for i in range(0, ROWS*2+1):
-                    sys.stdout.write("\x1b[F")
+                    sys.stdout.write("\x1b[F") # Move the cursor up to the top of the grid
                 for row in game_grid:
-                    for i in range(0, COLS):
+                    for i in range(0, COLS): # Print road markers
                         print("--", end="")
                     print()
 
                     for i in row:
-                        print(elements[i], end="")
+                        print(elements[i], end="") # Print the elements of the game (obstacle, car, air)
                     print()
 
                 for i in range(0, COLS):
-                    print("--", end="")
+                    print("--", end="") # Print final road markers
                 print()
         else:
             break
 
-    
+# Function for selecting the wanted connection    
 def connection_selection_func(kill_queue: mp.Queue, name: str, connection_selection, restart=False):
     global conn_det
-    if connection_selection == "connect":
-            if not restart:
+    if connection_selection == "connect": # If we want to connect to an existing server
+            if not restart: # If we are joining a new game
                 conn_det = input("Please specify the IP address and port, separated by a comma: ").split(",")
 
             winsound.PlaySound(None, winsound.SND_PURGE)
             print(f"Good luck, {name}!")
 
+            # Start the game
             socket: sock.SocketType = ConnectToServer(conn_det[0], int(conn_det[1]), name)
             CheckKeys(socket, kill_queue, name)
 
-    elif connection_selection == "new server":
+    elif connection_selection == "new server": # Otherwise if we want to start a new server
             print(f"Good luck, {name}!")
-            server_process = mp.Process(target=Server.CreateNewServer, args=(6089,kill_queue))
+            server_process = mp.Process(target=Server.CreateNewServer, args=(6089,kill_queue)) # Start a new server on another thread
             server_process.start()
 
             winsound.PlaySound(None, winsound.SND_PURGE)
-            socket: sock.SocketType = ConnectToServer("localhost", 6089, name)
+            socket: sock.SocketType = ConnectToServer("localhost", 6089, name) # Create a client to connect to our new server
             CheckKeys(socket, kill_queue, name)
 
-    else:
+    else: # Try again if garbage input
             print("Invalid input. Please try again.")
             main()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Main function that starts
     def main(kill_q: mp.Queue = None, name_par: str = None, connect_selec = None, res = False):
         global elements
         kill_queue = kill_q or mp.Queue()
+        # Initial music
         winsound.PlaySound("assets/intro.wav", winsound.SND_FILENAME + winsound.SND_ASYNC + winsound.SND_LOOP)
         print("Welcome to",
               "\033[36m" "A"
@@ -179,29 +189,29 @@ r'''
         "Remember, if you press the Up arrow key, and the other player presses something else, the car will not move.\n"
         "However, if you two press the same button at the same time, then the car will move.")
         
-        name = name_par or input("What is your name? ")
+        name = name_par or input("What is your name? ") # Check if the name already exists (if restarting)
 
         current_car_selection = 0
 
         print('\033[?25l', end="")
 
-        if not res:
+        if not res: # If we aren't restarting
             print("What car would you like?")
             print(f'<< {car_select_array[current_car_selection]} >>', end='\r')
             while True:
                 if msvcrt.kbhit():
                     character = msvcrt.getch()
                     if character == b"\xe0" or character == b"\x00":
-                        character = msvcrt.getch()
+                        character = msvcrt.getch() # Collect key inputs
 
-                        if character == b"M":
+                        if character == b"M": # Right arrow key
                             try:
-                                current_car_selection = min(current_car_selection+1, car_select_array.__len__()-1)
+                                current_car_selection = min(current_car_selection+1, car_select_array.__len__()-1) # choose whichever is lower to not go too far
                             except:
                                 pass
-                        elif character == b"K":
+                        elif character == b"K": # Left arrow key
                             try:
-                                current_car_selection = max(current_car_selection-1, 0)
+                                current_car_selection = max(current_car_selection-1, 0) # choose whatever is higher to not go too far
                             except:
                                 pass
 
@@ -210,7 +220,7 @@ r'''
                         break
         print(f"<< {car_select_array[current_car_selection]} >>", end='\n')
 
-        elements[1] = car_select_array[current_car_selection]
+        elements[1] = car_select_array[current_car_selection] # Set the car element to the emoji we want
         print('\033[?25h', end="")
         
         connection_selection = connect_selec or input("Would you like to connect to a server, or create a new one? (Connect/New Server): ").lower()
@@ -219,7 +229,7 @@ r'''
 
         restart = input("Would you like to play another game with the same settings? (Y/N) ")
 
-        if restart.lower() == 'y':
+        if restart.lower() == 'y': # Check if we want to restart
             main(kill_queue, name, connection_selection, res=True)
         else:
             pass
@@ -227,6 +237,6 @@ r'''
         return
     main()
 
-    if game_ended:
+    if game_ended: # Press key to exit to avoid closing window immediately
         print()
         os.system("pause")
--- Server.py
+++ Server.py
@@ -9,10 +9,14 @@ import os
 import time
 import random
 
+# Import required libraries
+
 import classes.NumpyEncoder as NumpyEncoder
 
 ROWS, COLS = 6, 20
 CAR_SYMBOL, OBSTACLE_SYMBOL, EMPTY_SYMBOL = 1, 2, 0
+
+# constant variables for grid size and integer representations of grid
 
 executed = False
 
@@ -20,69 +24,72 @@ users = []
 
 current_user_input = [0, 0]
 
-
+# Start logging
 logger = logging.getLogger("server")
 logging.basicConfig(filename='server.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
+# Setup the socket selector for managing multiple clients
 sel = selectors.DefaultSelector()
 
-def start_game(queue: mp.Queue, keyQueue: mp.Queue, kill_queue: mp.Queue):
-    grid = np.full((ROWS, COLS), EMPTY_SYMBOL, dtype=int)
-    car_x_pos = 0
+# Function called when starting the actual game
+def start_game(queue: mp.Queue, keyQueue: mp.Queue, kill_queue: mp.Queue): # Takes arguments for the users, key inputs, and whether the game has been terminated
+    grid = np.full((ROWS, COLS), EMPTY_SYMBOL, dtype=int) # Define a new NumPy array for the game filled with 0s
+    car_x_pos = 0 # The car starts at (0,1)
     car_y_pos = 1
 
-    grid[car_y_pos, car_x_pos] = CAR_SYMBOL
+    grid[car_y_pos, car_x_pos] = CAR_SYMBOL # Set this position on the grid
     
-    users = queue.get()
+    users = queue.get() # Get all the users currently connected
 
-    score = 0
+    score = 0 # Set the score initially to zero
 
     while True:
 
-        if kill_queue.empty() == False:
+        if kill_queue.empty() == False: # If the game has been terminated, exit the function and loop
             kill = kill_queue.get_nowait()
             if kill == True:
                 return
 
-        grid[random.randint(0,ROWS-1), -1] = random.choice([0,0,0,0,0,0,0,0,2])
-        grid[:, :-1] = grid[:, 1:]
+        grid[random.randint(0,ROWS-1), -1] = random.choice([0,0,0,0,0,0,0,0,2]) # Put an obstacle on the last column of a random row
+        grid[:, :-1] = grid[:, 1:] # Shift all obstacles one unit to the left
 
-        grid[car_y_pos, car_x_pos] += CAR_SYMBOL
+        grid[car_y_pos, car_x_pos] += CAR_SYMBOL # Re-set the car position after left shift
 
-        Kqueue = [0, 0]
+        Kqueue = [0, 0] # Define a local variable for key inputs
         
-        if keyQueue.qsize() > 0:
-            Kqueue = keyQueue.get_nowait()
+        if keyQueue.qsize() > 0: # If there is a key input
+            Kqueue = keyQueue.get_nowait() # Get all keys (avoid errors)
 
-        if Kqueue[0] == 1 and Kqueue[1] == 1:
-            if car_y_pos > 0:
+        if Kqueue[0] == 1 and Kqueue[1] == 1: # If both users click up arrows
+            if car_y_pos > 0: # Make sure we don't go too far up
+                grid[car_y_pos, car_x_pos] = EMPTY_SYMBOL # Set the old car position to empty
+                car_y_pos -= 1 # Move the car up
+                grid[car_y_pos, car_x_pos] += CAR_SYMBOL # Add the value of the car to the value of that position
+                # If the value is 3 or greater, than an obstacle exists in that position
+        elif Kqueue[0] == 2 and Kqueue[1] == 2: # If both users click down arrows
+            if car_y_pos < ROWS - 1: # Make sure we don't go too far down
                 grid[car_y_pos, car_x_pos] = EMPTY_SYMBOL
-                car_y_pos -= 1
-                grid[car_y_pos, car_x_pos] += CAR_SYMBOL
-        elif Kqueue[0] == 2 and Kqueue[1] == 2:
-            if car_y_pos < ROWS - 1:
-                grid[car_y_pos, car_x_pos] = EMPTY_SYMBOL
-                car_y_pos += 1
+                car_y_pos += 1 # Likewise
                 grid[car_y_pos, car_x_pos] += CAR_SYMBOL
 
-        if grid[car_y_pos, car_x_pos] >= 3:
-            for user in users:
+        if grid[car_y_pos, car_x_pos] >= 3: # If the car is colliding with an obstacle
+            for user in users: # Tell all users that they have lost
                 user[1].setblocking(False)
                 user[1].sendall(b"LOSE " + bytes(str(score), "utf-8"))
-                kill_queue.put_nowait(True)
-            os._exit(0)
+                kill_queue.put_nowait(True) # Turn the kill queue True, and communicate to all processes
+            os._exit(0) # Exit this thread
 
         score += 1
 
         user: dict
-        for user in users:
+        for user in users: # Send the new grid to users
             user[1].sendall((json.dumps(grid, cls=NumpyEncoder.NumpyEncoder) + "\n").encode("utf-8"))
         
 
-        time.sleep(0.3)
+        time.sleep(0.3) # Constant time change
 
 
-def accept(sock: sock.SocketType, mask, queue: mp.Queue, keyQueue: mp.Queue):
+def accept(sock: sock.SocketType, mask, queue: mp.Queue, keyQueue: mp.Queue): # Accept connections from client, send to read function
     conn, addr = sock.accept()  # Should be ready
 
     logger.info('[SERVER] Accepted %s from %s', conn, addr)
@@ -90,38 +97,38 @@ def accept(sock: sock.SocketType, mask, queue: mp.Queue, keyQueue: mp.Queue):
     conn.setblocking(False)
     sel.register(conn, selectors.EVENT_READ, read)
 
-def read(conn: sock.SocketType, mask, queue: mp.Queue, keyQueue: mp.Queue):
+def read(conn: sock.SocketType, mask, queue: mp.Queue, keyQueue: mp.Queue): # Read messages from clients
     global current_user_input
     data = conn.recv(4096)
     if data:
         logger.info('[SERVER] Received %s from %s', repr(data), conn)
 
-        if data.decode("utf-8").startswith("INIT"):
+        if data.decode("utf-8").startswith("INIT"): # If users first connecting, set their username and socket info in users list
             users.append([data.decode("utf-8")[4:], conn])
 
 
-        logger.info(data.decode("utf-8"))
+        logger.info(data.decode("utf-8")) # Log everything into file
 
         try:
-            while True:
+            while True: # Empty the key queue to set new inputs
                 keyQueue.get_nowait()
         except:
             pass
 
-        if data.decode("utf-8") == "UP":
-            if conn == users[0][1]:
+        if data.decode("utf-8") == "UP": # If somebody sent a message indicating they want to go up
+            if conn == users[0][1]: # Check who it is
                 current_user_input[0] = 1
             elif conn == users[1][1]:
                 current_user_input[1] = 1
-            keyQueue.put_nowait(current_user_input)
-        elif data.decode("utf-8") == "DOWN":
+            keyQueue.put_nowait(current_user_input) # Throw it into the queue for the next game loop
+        elif data.decode("utf-8") == "DOWN": # vice versa
             if conn == users[0][1]:
                 current_user_input[0] = 2
             elif conn == users[1][1]:
                 current_user_input[1] = 2
             keyQueue.put_nowait(current_user_input)
         else:
-            current_user_input = [0, 0]
+            current_user_input = [0, 0] # Else reset the queue. Nobody wants to do anything
             keyQueue.put_nowait(current_user_input)
 
     else:
@@ -132,39 +139,40 @@ def read(conn: sock.SocketType, mask, queue: mp.Queue, keyQueue: mp.Queue):
 
 
 def Server(sock: sock.SocketType, kill_queue: mp.Queue):
-    sel.register(sock, selectors.EVENT_READ, accept)
-    q = mp.Queue()
+    sel.register(sock, selectors.EVENT_READ, accept) # Start the selectors
 
+    # Establish different multiprocessing queues
+    q = mp.Queue()
     keyQueue = mp.Queue()
     
     
 
     while True:
-        events = sel.select()
+        events = sel.select() # When an event happens with the sockets
         for key, mask in events:
-            callback = key.data
-            callback(key.fileobj, mask, q, keyQueue)
+            callback = key.data # Get the function for the respective event (e.g. accept or read)
+            callback(key.fileobj, mask, q, keyQueue) # Call the function with the correct parameters
 
         global executed
-        if users.__len__() == 2 and executed == False:
+        if users.__len__() == 2 and executed == False: # Start the game
             q.put_nowait(users)
             executed = True
             mp.Process(target=start_game, args=(q, keyQueue, kill_queue)).start()
 
-        if kill_queue.empty() == False:
+        if kill_queue.empty() == False: # If there is something in the kill queue, it's time to terminate.
             kill = kill_queue.get_nowait()
             if kill == True:
                 return
 
 
-def CreateNewServer(port, kill_queue: mp.Queue):
+def CreateNewServer(port, kill_queue: mp.Queue): # Client-available function for starting the server
     serversocket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-    serversocket.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
-    serversocket.setblocking(False)
-    serversocket.bind(('', port))
+    serversocket.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1) # Reuse the port after crash or something goes wrong (e.g. restarting the game)
+    serversocket.setblocking(False) # Non-blocking socket for concurrency
+    serversocket.bind(('', port)) # Listen on all IPs
     serversocket.listen(5)
 
     logger.info("Server created on port %s.", str(port))
 
-    server_process = mp.Process(target=Server, args=(serversocket, kill_queue))
+    server_process = mp.Process(target=Server, args=(serversocket, kill_queue)) # Start the multiprocessing
     server_process.start()
